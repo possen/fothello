ec22841b009f79b065a5c9287a3e9dbd0a522a9a
Paul Ossenbruggen <possen@gmail.com>
2016-03-19 11:55:00 -0700

    setup for build server

--- Fothello/MatchViewController.m
+++ Fothello/MatchViewController.m
@@ -196,7 +196,7 @@
     return YES;
 }
 
-- (NSUInteger)supportedInterfaceOrientations
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations
 {
     if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
         return UIInterfaceOrientationMaskAllButUpsideDown;
