42f75c5d195b7a81e17d719998b1a60e818d971e
Ubuntu <ubuntu@ip-172-31-26-221.us-west-2.compute.internal>
2016-04-01 04:42:14 +0000

    commit build for server

---
+++ web/echo.c
@@ -0,0 +1,55 @@
+#include <fcgi_stdio.h>
+#include <stdlib.h>
+
+/* some of the HTTP variables we are interest in */
+#define MAX_VARS 30
+char* vars[MAX_VARS] = {
+    "DOCUMENT_ROOT",
+    "GATEWAY_INTERFACE",
+    "HTTP_ACCEPT",
+    "HTTP_ACCEPT_ENCODING",
+    "HTTP_ACCEPT_LANGUAGE",
+    "HTTP_CACHE_CONTROL",
+    "HTTP_CONNECTION",
+    "HTTP_HOST",
+    "HTTP_PRAGMA",
+    "HTTP_RANGE",
+    "HTTP_REFERER",
+    "HTTP_TE",
+    "HTTP_USER_AGENT",
+    "HTTP_X_FORWARDED_FOR",
+    "PATH",
+    "QUERY_STRING",
+    "REMOTE_ADDR",
+    "REMOTE_HOST",
+    "REMOTE_PORT",
+    "REQUEST_METHOD",
+    "REQUEST_URI",
+    "SCRIPT_FILENAME",
+    "SCRIPT_NAME",
+    "SERVER_ADDR",
+    "SERVER_ADMIN",
+    "SERVER_NAME",
+    "SERVER_PORT",
+    "SERVER_PROTOCOL",
+    "SERVER_SIGNATURE",
+    "SERVER_SOFTWARE"
+};
+
+int main(void)
+{
+    int count = 0, i;
+    char *v;
+    while (FCGI_Accept() >= 0) {
+        printf("Content-type: text/plain\r\n\r\n"
+               "Request number %d\n", ++count);
+        for (i = 0; i < MAX_VARS; ++i) {
+            v = getenv(vars[i]);
+            if (v == NULL)
+                printf("%s: \n", vars[i]);
+            else
+                printf("%s: %s\n", vars[i], v);
+        }
+    }
+    return 0;
+}
