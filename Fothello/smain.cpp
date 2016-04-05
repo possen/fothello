8c8f4e4bd4a891a6cf52f5b7d502a58fd0ff18ad
Paul Ossenbruggen <possen@gmail.com>
2016-04-05 06:49:07 +0000

    web service working

--- Fothello/smain.cpp
+++ Fothello/smain.cpp
@@ -19,59 +19,95 @@ char player1, player2;
 #define HUMAN 1
 #define COMPUTER 2
 
-#include <fcgi_stdio.h>
+#include <fcgio.h>
 #include <stdlib.h>
+#include <string>
+#include <iostream>
+#include "board.hpp"
 
-/* some of the HTTP variables we are interest in */
-#define MAX_VARS 30
-const char* vars[MAX_VARS] = {
-  "DOCUMENT_ROOT",
-  "GATEWAY_INTERFACE",
-  "HTTP_ACCEPT",
-  "HTTP_ACCEPT_ENCODING",
-  "HTTP_ACCEPT_LANGUAGE",
-  "HTTP_CACHE_CONTROL",
-  "HTTP_CONNECTION",
-  "HTTP_HOST",
-  "HTTP_PRAGMA",
-  "HTTP_RANGE",
-  "HTTP_REFERER",
-  "HTTP_TE",
-  "HTTP_USER_AGENT",
-  "HTTP_X_FORWARDED_FOR",
-  "PATH",
-  "QUERY_STRING",
-  "REMOTE_ADDR",
-  "REMOTE_HOST",
-  "REMOTE_PORT",
-  "REQUEST_METHOD",
-  "REQUEST_URI",
-  "SCRIPT_FILENAME",
-  "SCRIPT_NAME",
-  "SERVER_ADDR",
-  "SERVER_ADMIN",
-  "SERVER_NAME",
-  "SERVER_PORT",
-  "SERVER_PROTOCOL",
-  "SERVER_SIGNATURE",
-    "SERVER_SOFTWARE"
-};
+using namespace std;
+
+// Maximum bytes
+const unsigned long STDIN_MAX = 1000000;
+
+/**
+ * Note this is not thread safe due to the static allocation of the
+ * content_buffer.
+ */
+string get_request_content(const FCGX_Request & request) {
+  char * content_length_str = FCGX_GetParam("CONTENT_LENGTH", request.envp);
+  unsigned long content_length = STDIN_MAX;
+
+  if (content_length_str) {
+    content_length = strtol(content_length_str, &content_length_str, 10);
+    if (*content_length_str) {
+      cerr << "Can't Parse 'CONTENT_LENGTH='"
+	   << FCGX_GetParam("CONTENT_LENGTH", request.envp)
+	   << "'. Consuming stdin up to " << STDIN_MAX << endl;
+    }
+
+    if (content_length > STDIN_MAX) {
+      content_length = STDIN_MAX;
+    }
+  } else {
+    // Do not read from stdin if CONTENT_LENGTH is missing
+    content_length = 0;
+  }
+
+  char * content_buffer = new char[content_length];
+  cin.read(content_buffer, content_length);
+  content_length = cin.gcount();
+
+  // Chew up any remaining stdin - this shouldn't be necessary
+  // but is because mod_fastcgi doesn't handle it correctly.
+
+  // ignore() doesn't set the eof bit in some versions of glibc++
+  // so use gcount() instead of eof()...
+  do cin.ignore(1024); while (cin.gcount() == 1024);
+
+  string content(content_buffer, content_length);
+  delete [] content_buffer;
+  return content;
+}
 
 int main(void)
 {
-  int count = 0, i;
-  char *v;
-  while (FCGI_Accept() >= 0) {
-    printf("Content-type: text/plain\r\n\r\n"
-	   "Request number %d\n", ++count);
-    for (i = 0; i < MAX_VARS; ++i) {
-      v = getenv(vars[i]);
-      if (v == NULL)
-	printf("%s: \n", vars[i]);
-      else
-	printf("%s: %s\n", vars[i], v);
-    }
+  // Backup the stdio streambufs
+  streambuf * cin_streambuf  = cin.rdbuf();
+  streambuf * cout_streambuf = cout.rdbuf();
+  streambuf * cerr_streambuf = cerr.rdbuf();
+
+  FCGX_Request request;
+
+  FCGX_Init();
+  FCGX_InitRequest(&request, 0, 0);
+
+  while (FCGX_Accept_r(&request) == 0) {
+    fcgi_streambuf cin_fcgi_streambuf(request.in);
+    fcgi_streambuf cout_fcgi_streambuf(request.out);
+    fcgi_streambuf cerr_fcgi_streambuf(request.err);
+
+    cin.rdbuf(&cin_fcgi_streambuf);
+    cout.rdbuf(&cout_fcgi_streambuf);
+    cerr.rdbuf(&cerr_fcgi_streambuf);
+
+    const char * uri = FCGX_GetParam("REQUEST_URI", request.envp);
+
+    string content = get_request_content(request);
+    string jsonResp = getMoveFromJSON(content);
+
+    cout << "Content-type: application/json\r\n" 
+         << "\r\n"
+	 << jsonResp << "\r\n";
+
+    // Note: the fcgi_streambuf destructor will auto flush
   }
+
+  // restore stdio streambufs
+  cin.rdbuf(cin_streambuf);
+  cout.rdbuf(cout_streambuf);
+  cerr.rdbuf(cerr_streambuf);
+
   return 0;
 }
 
